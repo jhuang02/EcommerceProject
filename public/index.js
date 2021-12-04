f5b6ff6117d8c78776c13c051b6cd7425729366d
jhuang678 <jhuang678@gmail.com>
2021-12-03 17:43:37 -0800

    button switch view functionality, css styling like font, product flexbox, placeholder img

--- public/index.js
+++ public/index.js
@@ -4,9 +4,57 @@
   window.addEventListener("load", init);
 
   function init() {
-    console.log("HI");
     fetchExploreRandomProducts();
+    id("home-btn").addEventListener("click", homeView);
+    id("account-btn").addEventListener("click", accountView);
+    id("history-btn").addEventListener("click", historyView);
+    id("cart-btn").addEventListener("click", cartView);
+    id("search-btn").addEventListener("click", searchView);
+  }
 
+  function homeView() {
+    id("cart-view").classList.add("hidden");
+    id("search-view").classList.add("hidden");
+    id("account-view").classList.add("hidden");
+    id("history-view").classList.add("hidden");
+    id("feedback-view").classList.add("hidden");
+    id("home-view").classList.remove("hidden");
+  }
+
+  function accountView() {
+    id("cart-view").classList.add("hidden");
+    id("search-view").classList.add("hidden");
+    id("account-view").classList.remove("hidden");
+    id("history-view").classList.add("hidden");
+    id("feedback-view").classList.add("hidden");
+    id("home-view").classList.add("hidden");
+  }
+
+  function historyView() {
+    id("cart-view").classList.add("hidden");
+    id("search-view").classList.add("hidden");
+    id("account-view").classList.add("hidden");
+    id("history-view").classList.remove("hidden");
+    id("feedback-view").classList.add("hidden");
+    id("home-view").classList.add("hidden");
+  }
+
+  function cartView() {
+    id("cart-view").classList.remove("hidden");
+    id("search-view").classList.add("hidden");
+    id("account-view").classList.add("hidden");
+    id("history-view").classList.add("hidden");
+    id("feedback-view").classList.add("hidden");
+    id("home-view").classList.add("hidden");
+  }
+
+  function searchView() {
+    id("cart-view").classList.add("hidden");
+    id("search-view").classList.remove("hidden");
+    id("account-view").classList.add("hidden");
+    id("history-view").classList.add("hidden");
+    id("feedback-view").classList.add("hidden");
+    id("home-view").classList.add("hidden");
   }
 
   function fetchExploreRandomProducts() {
@@ -27,7 +75,8 @@
     // placeholder for now
     for (let i = 0; i < 10; i++) {
       let productArticle = generateProductArticle(i);
-      id("home-view").appendChild(productArticle);
+      productArticle.classList.add("product-card");
+      id("product-container").appendChild(productArticle);
     }
 
 
@@ -36,8 +85,12 @@
   function generateProductArticle(num) {
     let article = gen("article");
     let text = gen("p");
+    let img = gen("img");
+    img.classList.add("product-img");
+    img.src = "img/toaster.jpg";
     text.textContent = "Product " + num;
     article.appendChild(text);
+    article.appendChild(img);
     return article;
   }
 
