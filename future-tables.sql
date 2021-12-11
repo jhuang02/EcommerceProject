7699a676c5ff1bcf05edb91848aa76b8497b92c3
Justin Yang <justinyg1209@gmail.com>
2021-12-10 17:37:32 -0800

    added database

---
+++ future-tables.sql
@@ -0,0 +1,35 @@
+CREATE TABLE shopping (
+  username INTEGER,
+  productid INTEGER,
+  cartId INTEGER,
+  quantity INTEGER,
+  FOREIGN KEY(productid) references product(id)
+);
+
+CREATE TABLE user (
+  username TEXT,
+  password TEXT,
+  cartId INTEGER
+);
+
+CREATE TABLE product (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  quantity INTEGER,
+  name TEXT,
+  category TEXT,
+  price DECIMAL(10, 5)
+);
+
+CREATE TABLE feedback (
+  productid INTEGER,
+  rating INTEGER,
+  count INTEGER,
+  reviews TEXT,
+  FOREIGN KEY(productid) references product(id)
+);
+
+CREATE TABLE history (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  userid INTEGER,
+  FOREIGN KEY(userid) references user(id)
+);
