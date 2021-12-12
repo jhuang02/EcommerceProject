3690d69f7abeb2655d2586e1e396a3b7477c2bb3
Justin Yang <justinyg1209@gmail.com>
2021-12-11 22:01:57 -0800

    started fixing CSS errors

--- public/index.js
+++ public/index.js
@@ -573,11 +573,11 @@ function finishCartView(totalCost) {
     buyBtn.textContent = 'Add to Cart!';
 
     buyBtn.addEventListener('click', purchaseItem);
-    if (resp['quantity'] === 0) {
+    if (res['quantity'] === 0) {
       buyBtn.disabled = true;
     }
-    priceElement.textContent = 'Price: ' + resp['price'];
-    fetch('/ecommerce/feedback?productId=' + resp['id'])
+    priceElement.textContent = 'Price: ' + res['price'];
+    fetch('/ecommerce/feedback?productId=' + res['id'])
       .then(statusCheck)
       .then(res => res.json())
       .then(res => {
